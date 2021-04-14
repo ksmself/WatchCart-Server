91a84cfc275ae78790c8aebce9e60784d8ee916f
insomenia-seongwon <jsw2095@insomenia.com>
2021-04-14 17:39:43 +0900

    fix generator

--- lib/generators/api/api_generator.rb
+++ lib/generators/api/api_generator.rb
@@ -2,18 +2,14 @@ class ApiGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
 
   def generate_controller
-    template 'controller.rb', "app/controllers/#{name}_controller.rb"
-    template 'test.rb', "test/controllers/#{name}_controller_test.rb"
+    template 'controller.rb', "app/controllers/#{name.pluralize}_controller.rb"
+    template 'test.rb', "test/controllers/#{name.pluralize}_controller_test.rb"
   end
 
   private
 
   def api_name
-    if name.pluralize.singularize == name
-      "#{name.classify}"
-    else
-      "#{name.classify}s"
-    end
+    name.pluralize.split('/').map(&:capitalize).join('::')
   end
 
   def version
