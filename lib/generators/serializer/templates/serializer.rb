5ca0d8e4a97b37e056c5efacf40fd3f33bd4030b
insomenia-seongwon <jsw2095@insomenia.com>
2021-05-16 22:55:44 +0900

    generator fix

--- lib/generators/serializer/templates/serializer.rb
+++ lib/generators/serializer/templates/serializer.rb
@@ -1,3 +1,3 @@
-class <%= serializer_name %>Serializer
+class <%= serializer_name %>Serializer < Panko::Serializer
   attributes <% attributes.each_with_index do |attr, index| -%>:<%= attr %><%= (index != attributes.count - 1) ? ',' : '' %> <% end %>
 end
