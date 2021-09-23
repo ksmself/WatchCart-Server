2f1bcd25b5adc1946bcc5d2771e9973a753a5fb7
ksmself <53217988+ksmself@users.noreply.github.com>
2021-09-23 13:46:01 +0900

    ADD: has_one option, movie for line_item

--- app/serializers/line_item_each_serializer.rb
+++ app/serializers/line_item_each_serializer.rb
@@ -1,3 +1,5 @@
 class LineItemEachSerializer < Panko::Serializer
   attributes :id, :option_id, :order_id, :quantity, :status
+
+  has_one :option, serializer: OptionEachSerializer
 end
--- app/serializers/option_each_serializer.rb
+++ app/serializers/option_each_serializer.rb
@@ -1,3 +1,5 @@
 class OptionEachSerializer < Panko::Serializer
-  attributes :id, :movie_id, :name 
+  attributes :id, :movie_id, :name, :price
+
+  has_one :movie, serializer: MovieEachSerializer
 end
