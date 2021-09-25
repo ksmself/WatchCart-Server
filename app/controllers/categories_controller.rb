4f56d9774b04fa3338528d87a303cb20606e68f1
ksmself <53217988+ksmself@users.noreply.github.com>
2021-09-25 13:52:34 +0900

    ADD: Cateogory.movies sorting by stars

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,7 @@
 class CategoriesController < ApiController
+  before_action :set_category, only: [:show, :update, :destroy]
+  before_action :set_query_param, only: [:show]
+
   def index
     if request.path === '/categories'
       categories = Category.all
@@ -10,7 +13,33 @@ class CategoriesController < ApiController
   end
 
   def show
-    category = Category.find(params[:id])
-    render json: serialize(category)
+    if @queryParam.nil?
+      sortedMovies = @category.movies
+    else 
+      if @queryParam[:s] === 'stars desc'
+        sortedMovies = @category.movies.sort{ |a, b| b.stars <=> a.stars } 
+      else 
+        sortedMovies = @category.movies.sort{ |a, b| a.stars <=> b.stars } 
+      end
+    end
+    render json: {
+      id: @category.id, 
+      title: @category.title,
+      movies: sortedMovies, 
+    }
+  end
+
+  private 
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def set_query_param
+    @queryParam = request.query_parameters[:q]
+  end
+
+  def permitted_query
+    params[:q].permit(:s)
   end
 end
