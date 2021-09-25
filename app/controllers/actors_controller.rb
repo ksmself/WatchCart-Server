f2b661f1e54596cd205c5848125619623866dc3d
ksmself <53217988+ksmself@users.noreply.github.com>
2021-09-25 14:04:26 +0900

    ADD: Actor.played_movies sorting by stars

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,6 @@
 class ActorsController < ApiController
     before_action :set_actor, only: [:show]
+    before_action :set_query_param, only: [:show]
 
     def index 
         #actors = Actor.ransack(params[:q]).result
@@ -13,7 +14,20 @@ class ActorsController < ApiController
     end 
 
     def show
-        render json: serialize(@actor)
+        if @queryParam.nil?
+            sortedMovies = @actor.played_movies
+        else
+            if @queryParam[:s] === 'stars desc'
+                sortedMovies = @actor.played_movies.sort{ |a, b| b.stars <=> a.stars } 
+            else
+                sortedMovies = @actor.played_movies.sort{ |a, b| a.stars <=> b.stars } 
+            end
+        end
+        render json: {
+            id: @actor.id, 
+            name: @actor.name, 
+            movies: sortedMovies,
+        }
     end
 
     private 
@@ -24,6 +38,10 @@ class ActorsController < ApiController
 
     def set_actor 
         @actor = Actor.find(params[:id])
+    end
+
+    def set_query_param
+        @queryParam = request.query_parameters[:q]
     end
 
     def permitted_query
