9f18ba96aed68c54a45c6b90138093ad35016a0e
ksmself <53217988+ksmself@users.noreply.github.com>
2021-09-28 15:50:37 +0900

    ADD: UserPasswordUpdate success/fail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,28 +6,18 @@ class UsersController < ApiController
         render json: serialize(user)
     end
 
-    # def update 
-    #     # params
-    #       # name, email, curPassword, newPassword
-    #     # curPassword와 user의 비밀번호가 일치하면
-    #       # name 변경
-    #       # newPassword가 ''이 아니면 변경 
-    #       if @user["password"] == user_params[:curPassword]
-    #         @user["name"] = user_params[:name]
-    #         if user_params[:newPassword] != ''
-    #           @user["password"] = user_params[:newPassword]
-    #           render json: serialize(@user)
-    #         end
-    #     # curPassword와 user의 비밀번호 일치하지 않는다면 에러 리턴
-    #       else
-    #         render json: serialize(@user)
-    #       end
-    # end 
+    def update
+        if @user.update_with_password(user_params)
+            render json: true 
+        else
+            render json: {error: "현재 비밀번호를 다시 확인해주세요" }
+        end 
+    end
   
     private 
   
     def user_params
-        params.require(:user).permit(:name, :email, :curPassword, :newPassword, :newPassword_confirmation, :liked_movies, :orders)
+        params.require(:user).permit(:name, :email, :current_password, :password, :password_confirmation, :liked_movies, :orders)
     end 
   
     def set_user 
